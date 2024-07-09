9ff76c31c32f38f940f4c0d9f9acad7f60b1ad60
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-09 13:41:22 +0000

    added a main menu

--- main.lua
+++ main.lua
@@ -1,4 +1,19 @@
 guessgames = require("guessgames")
 print("Welcome to the simple games program")
 
-guessgames.number_guessor()
+function menu()
+  print("main menu")
+  print("1. Number Guessor")
+  print("0. Exit")
+  print("Enter your choice:")
+  local choice = io.read()
+  if choice == "1" then
+     guessgames.number_guessor()
+  elseif choice == "0" then
+     print("exiting")
+  else
+     print("exiting")
+  end
+end
+
+
