3022020b4d9e2e7529f277d989f63066c8237037
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-10 12:07:33 +0000

    added words and how to access them

--- games/hangman.lua
+++ games/hangman.lua
@@ -4,9 +4,29 @@
   last update: july 10 2024
 ]]
 
-local hangmangame = {}
+local hangman = {}
+utilities = require("modules.utilities")
 
-function main()
+function set_word(words)
+  local sel_word = words[utilities.generate_number(1, #words)]
+  local x = ""
+  for i = 1, #sel_word do
+    x = x .. "_"
+  end
+  print(x.." starts with "..string.sub(sel_word, 1, 1))
+  return {sel_word, x}
 end
 
-return hangmangame
+function load_game()
+end
+
+function play()
+end
+
+function hangman.main()
+  print("welcome to hangman")
+  local word = utilities.get_data("./modules/1000_words.txt")
+  local phrase = set_word(word)
+end
+
+return hangman
--- modules/utilities.lua
+++ modules/utilities.lua
@@ -10,4 +10,18 @@ function utilities.generate_number(min, max)
   return math.random(min, max)
 end
 
+function utilities.get_data(filepath)
+  local wrd = {}
+  local file = io.open(filepath, "r")
+  if file then
+    for line in file:lines() do
+      table.insert(wrd, line)
+    end
+    file:close()
+  else
+    print("error in opening file")
+  end
+  return wrd
+end
+
 return utilities
