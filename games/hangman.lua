d2d3a415d86e4a9478164aa67c04e74199b0cc72
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-10 13:57:24 +0000

    hangman works

--- games/hangman.lua
+++ games/hangman.lua
@@ -13,20 +13,42 @@ function set_word(words)
   for i = 1, #sel_word do
     x = x .. "_"
   end
-  print(x.." starts with "..string.sub(sel_word, 1, 1))
   return {sel_word, x}
 end
-
-function load_game()
+           
+function load_game(word)
+  local phrase = set_word(word)
+  play(phrase[1], phrase[2])
 end
 
-function play()
+function play(word, dashes)
+  if #word > 4 then
+    local hint = utilities.generate_number(1, #word)
+    print("the hint is "..string.sub(word, hint, hint))
+  end
+  print(word)
+  for i = 1, #word do
+    while string.lower(string.sub(word, i, i)) ~= string.lower(string.sub(dashes, i, i)) do 
+      print(dashes)
+      local guess = io.read()
+      if string.lower(string.sub(word, i, i)) == string.lower(string.sub(guess, 1, 1)) then 
+        print("you got it correct")
+        if i < #word then
+          dashes = string.sub(dashes, 1, i-1) .. string.lower(string.sub(guess, 1, 1)) .. string.sub(dashes, i+1, #dashes)
+        else
+          dashes = string.sub(dashes, 1, i-1) .. string.lower(string.sub(guess, 1, 1))
+        end  
+      else
+        print("you got it wrong")
+      end
+    end
+  end
 end
 
 function hangman.main()
   print("welcome to hangman")
   local word = utilities.get_data("./modules/1000_words.txt")
-  local phrase = set_word(word)
+  load_game(word)
 end
 
 return hangman
